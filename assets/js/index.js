8455aa7a1809606433921aa5eea40b8ac7878451
paulo-xavier <px568661@gmail.com>
2023-12-11 00:48:37 -0300

    :tada: FEAT

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const hamburguerMenu = document.querySelector('.hamburguer'); 
+const containerMenu = document.querySelector('#container-menu'); 
+const navLink = document.querySelectorAll('.nav-link')
+
+hamburguerMenu.addEventListener('click', () => {
+
+    containerMenu.classList.toggle('container-menu--open'); 
+    hamburguerMenu.classList.toggle('hamburguer--open');
+});
+
+navLink.forEach(e => e.addEventListener('click', () => {
+    containerMenu.classList.remove('container-menu--open'); 
+    hamburguerMenu.classList.remove('hamburguer--open');
+})); 
